d33daf10456dc378c888bf495787164f3047494d
Luong-Minh2012 <luonggiaminh20@gmail.com>
2025-07-06 18:31:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const offBtn = document.getElementById("offBtn");
 const stopBtn = document.getElementById("stopBtn");
 
 let autoSwap = null;
-const Esp = "";
-function setToEsp(path) {
-  fetch(`${Esp}/${path}`)
-    .then((res) => console.log("Đã gửi lệnh: ", path))
-    .catch((error) => console.error("Lỗi gửi đến: ", error));
-}
+// const Esp = "";
+// function setToEsp(path) {
+//   fetch(`${Esp}/${path}`)
+//     .then((res) => console.log("Đã gửi lệnh: ", path))
+//     .catch((error) => console.error("Lỗi gửi đến: ", error));
+// }
 function setActive(button) {
   if (autoSwap) {
     clearTimeout(autoSwap);
@@ -19,12 +19,12 @@ function setActive(button) {
     onBtn.classList.add("active");
     offBtn.classList.remove("active");
     console.log("Bật");
-    setToEsp("on");
+    // setToEsp("on");
   } else {
     offBtn.classList.add("active");
     onBtn.classList.remove("active");
     console.log("Tắt");
-    setToEsp("off");
+    // setToEsp("off");
   }
   autoSwap = setTimeout(() => {
     setActive(button === "on" ? "off" : "on");
